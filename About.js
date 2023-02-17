8ae048b3d52f7ea21da691bcce32a21981934703
RayOmiyi <100414922+RayOmiyi@users.noreply.github.com>
2023-02-17 14:38:33 +0100

    Add files via upload

---
+++ About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About=()=>{
+  return(
+    <section className="section about-section">
+    <h2 className="section-title">about us</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam quasi delectus soluta eligendi exercitationem earum vero ratione, illo debitis! Omnis quia neque earum. Quibusdam minima id error dolor dolorum fuga impedit. Laboriosam impedit, aliquid illum quam, iste, dolorem similique error animi ipsum mollitia quia ex tempora. Necessitatibus incidunt illo amet.</p>
+    </section>
+  )
+}
+export default About
